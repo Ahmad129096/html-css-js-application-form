c070314ddcc0a3e865689b8bc7a1bd6fe09fddbc
Ahmad129096 <72014036+Ahmad129096@users.noreply.github.com>
2020-10-10 01:55:33 +0500

    Update form.js

--- form.js
+++ form.js
@@ -16,14 +16,14 @@ function AddData() {
              
 
 
-    // if(name==""|| gender==""||age==""||city=="")
-    // {
-    //     alert("No field should be empty")
+    if(name==""|| gender==""||age==""||city=="")
+    {
+        alert("No field should be empty")
         
-    // }
+    }
             
-    //      else
-    //      { 
+         else
+         { 
             rows += "<td>" + name + "</td><td>" + gender + "</td><td>" + age + "</td><td>" + city   + "</td><td><input type='button' onclick='edit_row()' value='Update' id='upd-btn'>/<input type='button' value='Remove' onclick='deleteRow(this)' id='del-btn'></td>"
             var tbody = document.querySelector("#list tbody");
             var tr = document.createElement("tr");
@@ -34,7 +34,7 @@ function AddData() {
        
     
 
-    //       }
+          }
         
 
             
